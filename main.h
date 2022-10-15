cee0e8176de85a2fc367e2d68fe9a19d826e4fcb
Jay stones <Joelpaul345@gmail.com>
2022-10-15 23:37:57 +0100

    Main.h

--- main.h
+++ main.h
@@ -6,19 +6,21 @@
 #include <stdarg.h>
 #include <limits.h>
 #include <unistd.h>
-#include <stddef.h>
+
+
 
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @p: type pointer to function for the conversion specifier
+ * @f: type pointer to function for the conversion specifier
  *
  */
+
 typedef struct format
 {
 	char *id;
-	int (*p)();
-} struct_type;
+	int (*f)();
+} convert_value;
 
 int printf_pointer(va_list val);
 int printf_hex_aux(unsigned long int num);
@@ -43,6 +45,5 @@ int printf_char(va_list val);
 int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
-
 
 #endif /*MAIN_H*/
