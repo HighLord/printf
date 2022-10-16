903c9c12747faa4ccef33b5ccfa0db46058389d1
Jay stones <Joelpaul345@gmail.com>
2022-10-16 02:26:30 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -8,11 +8,9 @@
  * Return: a pointer
  * structype selector - Struct
  */
-
 int (*driver(char *format))(char *format, va_list)
 {
 int i;
-
 struct_type selector[] = {
 {"%c", printc},
 {"%s", printstr},
@@ -24,7 +22,6 @@ struct_type selector[] = {
 {"%o", printocta},
 {NULL, NULL},
 };
-
 if (format[1] == ' ' || format[1] == '\0')
 return (NULL);
 for (i = 0; selector[i].q; i++)
