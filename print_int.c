2460af51796cbb4b9abe99a9079b65ee5a740e23
Jay stones <Joelpaul345@gmail.com>
2022-10-16 02:11:49 +0100

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -1,10 +1,15 @@
 #include "main.h"
+/**
+ * _abs - calculates the absolute value.
+ * @number: input.
+ * Return: value.
+ */
 int _abs(int number)
 {
 	if (number < 0)
-		return (-1 * number);
+	return (-1 * number);
 	else
-		return (number);
+	return (number);
 }
 
 /**
