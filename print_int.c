afbb90d4f66f7c3d7a50ba2e7ee644563359bdc3
Jay stones <Joelpaul345@gmail.com>
2022-10-16 02:18:51 +0100

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -6,12 +6,11 @@
  */
 int _abs(int number)
 {
-	if (number < 0)
-	return (-1 * number);
-	else
-	return (number);
+if (number < 0)
+return (-1 * number);
+else
+return (number);
 }
-
 /**
  * contadordigit - counts the digits.
  * @number: input integer
@@ -19,18 +18,16 @@ int _abs(int number)
  */
 int contadordigit(int number)
 {
-	int count = 0;
-	int number2 = number;
-
-	if (number <= 0)
-		count += 1;
-
-	while (_abs(number2) != 0))
-	{
-		number2 = number2 / 10;
-		count++;
-	}
-	return (count);
+int count = 0;
+int number2 = number;
+if (number <= 0)
+count += 1;
+while (_abs(number2) != 0))
+{
+number2 = number2 / 10;
+count++;
+}
+return (count);
 }
 /**
  * integer - a function that prints an integer.
@@ -39,25 +36,21 @@ int contadordigit(int number)
  */
 int integer(int number)
 {
-	unsigned int value;
-	int count;
-
-	count = contadordigit(number);
+unsigned int value;
+int count;
+count = contadordigit(number);
 if (number < 0)
 {
 _putchar('-');
 value = -number;
 }
-	else
-		value = number;
-
-	if (value >= 10)
-		integer(value / 10);
-	_putchar(value % 10 + '0');
-
-	return (count);
+else
+value = number;
+if (value >= 10)
+integer(value / 10);
+_putchar(value % 10 + '0');
+return (count);
 }
-
 /**
  * printint - prints a number
  * @format: format to print number
@@ -66,11 +59,9 @@ value = -number;
  */
 int printint(char *format, va_list pa)
 {
-	int number = va_arg(pa, int);
-	int num;
-	(void)format;
-
-	num = integer(number);
-
-	return (num);
+int number = va_arg(pa, int);
+int num;
+(void)format;
+num = integer(number);
+return (num);
 }
