64b96e88b313d26acc311c724163a56cf351d9cd
Jaaystones <Joelpaul345@gmail.com>
2022-10-17 09:13:58 -0700

    Printf project

--- handle_print.c
+++ handle_print.c
@@ -15,7 +15,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int p, unknow_len = 0, printed_chars = -1;
-	fma format_types[] = {
+	fmt_t format_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
--- main.h
+++ main.h
@@ -47,17 +47,17 @@ struct fomat
 
 /**
  *
- ** typedef struct fmt fma - Struct op
+ ** typedef struct fmt fmt_t - Struct op
  *
  **
  *
  ** @fmt: The format.
  *
- ** @fma: The function associated.
+ ** @fmt_t: The function associated.
  *
  **/
 
-typedef struct fmt fma;
+typedef struct fmt fmt_t;
 	int _printf(const char *format, ...);
 	int handle_print(const char *fmt, int *i,
 	va_list list, char buffer[], int flags, int width, int precision, int size);
